6cdaf656250d3c4fa9f9b386256285ab4c78b101
Shneor <mm4471661@gmail.com>
2025-02-11 10:20:39 +0200

    Merged all files into merged.py

---
+++ merged.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+def f3(a, b):
+    return a * b
+
+
+def f4(a, b):
+    return a / b
+def f5(a, b):
+    return a ** b
+
+
+def f6(a):
+    return a ** 0.5
